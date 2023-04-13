1f64f43cfb890c94d932d18422f5ef45dd75a62c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 12:58:56 -0300

    create location struct

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,8 +8,16 @@
 import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    var id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -3.71839, longitude: -38.5434), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
+    
     
     var body: some View {
         Map(coordinateRegion: $mapRegion)
