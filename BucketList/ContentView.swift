aa42b8873ebe3228ca2e2bcd25507c8e32ddbe06
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 13:24:36 -0300

    add a mapAnnotation so we can use any swiftui view we want

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -24,7 +24,11 @@ struct ContentView: View {
     
     var body: some View {
         Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-            MapMarker(coordinate: location.coordinate)
+            MapAnnotation(coordinate: location.coordinate) {
+                Circle()
+                    .stroke(.red, lineWidth: 3)
+                    .frame(width: 44, height: 44)
+            }
         }
     }
 }
