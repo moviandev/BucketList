7583f641100fb0f365b44823f626c331a40153c5
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 13:01:32 -0300

    create array of locations

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -15,9 +15,12 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -3.71839, longitude: -38.5434), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
-    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
         Map(coordinateRegion: $mapRegion)
