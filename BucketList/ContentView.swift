5b2b51326b25d88a1e4772f5b693243437bc653d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 12:55:25 -0300

    displaying map with the coordinates created

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -12,13 +12,7 @@ struct ContentView: View {
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.6, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map(coordinateRegion: $mapRegion)
     }
 }
 
