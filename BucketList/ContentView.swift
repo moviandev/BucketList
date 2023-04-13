f6ba28831b655ab84523c68886baaf1e748f3f3a
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 12:57:29 -0300

    change latitude and longitude

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,7 +9,7 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.6, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -3.71839, longitude: -38.5434), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
     var body: some View {
         Map(coordinateRegion: $mapRegion)
