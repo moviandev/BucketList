8c96dfb402a0a5c1fbeb828466ebc91f347dab43
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-13 00:03:31 -0300

    rollback

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,40 +7,15 @@
 
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
 struct ContentView: View {
-    var loadingState = LoadingState.loading
-    
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
