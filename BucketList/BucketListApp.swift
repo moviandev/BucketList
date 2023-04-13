7357319179edcd1a69754818f44f76ebcf16481a
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-12 23:30:15 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Matheus Viana on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
