ed1eec7a55a293d0e75dfed651a2e424dd49858b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-12 23:56:02 -0300

    Challenge

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,32 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Create a generic method to save into file like we did in project 8
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-                .onTapGesture {
-                    let str = "Text manager"
-                    let url = getDocumentsDirectory().appendingPathComponent("message.text")
-                    
-                    do {
-                        try str.write(to: url, atomically: true, encoding: .utf8)
-                        
-                        let input = try String(contentsOf: url)
-                        print(input)
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
         }
         .padding()
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
     }
 }
 
