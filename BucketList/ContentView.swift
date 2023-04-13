133168ebd7b80d7544258128cd108a8790f2f946
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-12 23:47:17 -0300

    Finding and returning the document directory to save data in to it

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -17,6 +17,11 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
