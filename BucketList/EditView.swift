b19d09a7cec8c43331796b32f6caa3298523059b
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-18 00:19:40 -0300

    create EditView with state wrappers

---
+++ BucketList/EditView.swift
@@ -0,0 +1,46 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Matheus Viana on 18/04/23.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    var location: Location
+    
+    @State private var name: String
+    @State private var description: String
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
+                }
+            }
+            .navigationTitle("Plave details")
+            .toolbar {
+                Button("Save") {
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    init(location: Location) {
+        self.location = location
+        
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(location: Location.example)
+    }
+}
