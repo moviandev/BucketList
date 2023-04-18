9a7e8f25ed8deacc0fd7ab8c750b932175823a32
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-18 00:26:58 -0300

    using EditView

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -63,7 +63,11 @@ struct ContentView: View {
             }
         }
         .sheet(item: $selectedPlace) { place in
-            Text(place.description)
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
+                }
+            }
         }
     }
 }
