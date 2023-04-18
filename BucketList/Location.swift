d71ad4e29d96bca963f8ebb0eb1915dd4f67b039
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-17 23:49:25 -0300

    Create Location struct

---
+++ BucketList/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  BucketList
+//
+//  Created by Matheus Viana on 17/04/23.
+//
+
+import Foundation
+
+struct Location: Identifiable, Codable, Equatable {
+    let id: UUID
+    var name: String
+    var description: String
+    let location: Double
+    let longitude: Double
+}
