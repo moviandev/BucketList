cb3b1b6fb259dacca3da82936e50a0fe7ffedf35
Matheus Viana <matheus_o_viana@hotmail.com>
2023-04-17 23:35:18 -0300

    rollback

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -9,37 +9,13 @@ import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    
-    
     var body: some View {
         VStack {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
+            Image(systemName: "globe")
+                .foregroundColor(.blue)
+            Text("Hello, world!")
         }
-        .onAppear(perform: authenticate)
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                if success {
-                    isUnlocked = true
-                } else {
-                    
-                }
-            }
-        } else {
-            
-        }
+        .padding()
     }
 }
 
