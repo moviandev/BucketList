f7cc7b24aaffbdf87ef1801e10ff49be974d6cf6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-02 21:09:03 -0300

    using enum to display our views depending on the state of our app

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct EditView: View {
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
     @Environment(\.dismiss) var dismiss
     var location: Location
     var onSave: (Location) -> Void
@@ -15,12 +19,31 @@ struct EditView: View {
     @State private var name: String
     @State private var description: String
     
+    @State private var loadingState = LoadingState.loading
+    @State private var pages: [Page] = [Page]()
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        Text("loading...")
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ")
+                            + Text("Page description here")
+                        }
+                    case .failed:
+                        Text("Please try again later")
+                    }
                 }
             }
             .navigationTitle("Plave details")
