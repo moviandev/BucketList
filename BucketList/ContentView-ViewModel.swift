689138d180fcfcc26d1d3f6d54399fafbe86a8e9
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-03 23:02:08 -0300

    Create viewmodel for contentView

---
+++ BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Matheus Viana on 03/05/23.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        
+    }
+}
