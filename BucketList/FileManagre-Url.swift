1cbc5c26b58ec00e5f3665f5e5e2b5c92488adf3
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-03 23:23:27 -0300

    change func to a static property

--- BucketList/FileManagre-Url.swift
+++ BucketList/FileManagre-Url.swift
@@ -8,9 +8,8 @@
 import Foundation
 
 extension FileManager {
-    func GetFileManagerUrl() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
+    static var DocumentDirectory: URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)        
         return paths[0]
     }
 }
