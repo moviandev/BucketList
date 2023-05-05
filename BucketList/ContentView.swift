b1c975b2baf5ce1187075c40048ac7060e7a5439
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-04 23:20:46 -0300

    move modifiers to image instead using on labels

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -49,14 +49,14 @@ struct ContentView: View {
                             viewModel.addLocation()
                         } label: {
                             Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .background(.ultraThinMaterial)
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
                         }
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .background(.ultraThinMaterial)
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
                     }
                 }
             }
